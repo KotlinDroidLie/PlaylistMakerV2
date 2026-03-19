b0e758505ee8ad195f7c8cf0a07fb5f8227cc2cb
Кифа Андрей <asdxasdx20022002@yandex.ru>
2026-03-19 15:12:41 +0300

    15.1.7
    - Добавлено кэширование HistoryTracksRepository в Creator:
      - Репозиторий создаётся только один раз при первом обращении
      - Последующие вызовы возвращают тот же экземпляр
    
    - Созданы методы для создание трех use case для управления историей поиска:
      - GetAddTrackToHistoryUseCase — добавление трека в историю
      - GetClearSearchHistoryUseCase — очистка всей истории
      - GetSearchHistoryUseCase — получение текущей истории
    
    - Все use case используют единый экземпляр HistoryTracksRepository

--- app/src/main/java/com/practicum/playlistmaker/Creator.kt
+++ app/src/main/java/com/practicum/playlistmaker/Creator.kt
@@ -2,16 +2,25 @@ package com.practicum.playlistmaker
 
 import android.content.Context
 import com.practicum.playlistmaker.data.network.RetrofitNetworkClient
+import com.practicum.playlistmaker.data.repository.LocalHistoryTracksRepository
 import com.practicum.playlistmaker.data.repository.LocalThemeRepositoryImpl
 import com.practicum.playlistmaker.data.repository.RemoteTrackRepositoryImpl
+import com.practicum.playlistmaker.domain.api.repo.HistoryTracksRepository
 import com.practicum.playlistmaker.domain.api.usecase.SearchTracksUseCase
 import com.practicum.playlistmaker.domain.impl.SearchTracksUseCaseImpl
 import com.practicum.playlistmaker.domain.api.usecase.SwitchThemeUseCase
 import com.practicum.playlistmaker.domain.api.repo.ThemeRepository
 import com.practicum.playlistmaker.domain.api.repo.TrackRepository
+import com.practicum.playlistmaker.domain.api.usecase.AddTrackToHistoryUseCase
+import com.practicum.playlistmaker.domain.api.usecase.ClearSearchHistoryUseCase
+import com.practicum.playlistmaker.domain.api.usecase.GetSearchHistoryUseCase
+import com.practicum.playlistmaker.domain.impl.AddTrackToHistoryUseCaseImpl
+import com.practicum.playlistmaker.domain.impl.ClearSearchHistoryUseCaseImpl
+import com.practicum.playlistmaker.domain.impl.GetSearchHistoryUseCaseImpl
 import com.practicum.playlistmaker.domain.impl.SwitchThemeUseCaseImpl
 
 object Creator {
+    private var historyRepository: HistoryTracksRepository? = null
     private fun getThemeRepository(context: Context): ThemeRepository {
         return LocalThemeRepositoryImpl(context)
     }
@@ -26,4 +35,23 @@ object Creator {
     fun getSearchTracksUseCase(): SearchTracksUseCase {
         return SearchTracksUseCaseImpl(getTrackRepository())
     }
+    private fun getHistoryRepository(context: Context): HistoryTracksRepository{
+        return historyRepository ?: LocalHistoryTracksRepository(context).also {
+            historyRepository = it
+        }
+    }
+
+    fun getAddTrackToHistoryUseCase(context: Context): AddTrackToHistoryUseCase{
+        return AddTrackToHistoryUseCaseImpl(getHistoryRepository(context))
+    }
+
+    fun getClearSearchHistoryUseCase(context: Context): ClearSearchHistoryUseCase {
+        return ClearSearchHistoryUseCaseImpl(getHistoryRepository(context))
+    }
+
+    fun getGetSearchHistoryUseCase(context: Context): GetSearchHistoryUseCase {
+        return GetSearchHistoryUseCaseImpl(getHistoryRepository(context))
+    }
+
+
 }
