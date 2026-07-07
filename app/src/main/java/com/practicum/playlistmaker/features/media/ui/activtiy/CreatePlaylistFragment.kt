05ab55627bc1048bd6e508a6cb3bd4ce694b2204
Кифа Андрей <asdxasdx20022002@yandex.ru>
2026-07-07 19:47:12 +0300

    22.1.4 feat: добавить логику экрана создания плейлиста
    - Добавлен OnBackPressedCallback с подтверждением выхода
    - Создан диалог confirmDialog при несохранённых данных
    - Создан PickVisualMedia для выбора обложки
    - Реализована активация кнопки при заполнении полей
    - Добавлен Snackbar с уведомлением о создании плейлиста

--- app/src/main/java/com/practicum/playlistmaker/features/media/ui/activtiy/CreatePlaylistFragment.kt
+++ app/src/main/java/com/practicum/playlistmaker/features/media/ui/activtiy/CreatePlaylistFragment.kt
@@ -4,7 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.snackbar.Snackbar
 import com.practicum.playlistmaker.databinding.FragmentCreatePlaylistBinding
 import com.practicum.playlistmaker.features.main.BottomNavHider
 
@@ -12,6 +19,40 @@ class CreatePlaylistFragment: Fragment() {
     private var _binding: FragmentCreatePlaylistBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var onBackCallBack: OnBackPressedCallback
+    private lateinit var confirmDialog: MaterialAlertDialogBuilder
+    private var isPosterSet = false
+
+    private val pickMedia = registerForActivityResult(ActivityResultContracts.PickVisualMedia()){ uri ->
+        uri?.let {
+
+            isPosterSet = true
+        }
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        confirmDialog = MaterialAlertDialogBuilder(requireContext())
+            .setTitle("Завершить создание плейлиста?")
+            .setMessage("Все несохраненные данные будут потеряны")
+            .setPositiveButton("Завершить") { dialog, which ->
+                findNavController().navigateUp()
+            }
+            .setNeutralButton("Отмена", null)
+
+        onBackCallBack = object : OnBackPressedCallback(true){
+            override fun handleOnBackPressed() {
+                val hasTitle = !binding.etTitle.text.isNullOrBlank()
+                val hasDescription = !binding.etDescription.text.isNullOrBlank()
+                val hasPoster = isPosterSet
+                if(hasTitle || hasDescription || hasPoster){
+                    showConfirmDialog()
+                } else{
+                    this.isEnabled = false
+                    requireActivity().onBackPressedDispatcher.onBackPressed()
+                }
+            }
+        }
+    }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -19,6 +60,25 @@ class CreatePlaylistFragment: Fragment() {
     ): View {
         _binding = FragmentCreatePlaylistBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, onBackCallBack)
+        binding.btnCreatePlaylistBack.setNavigationOnClickListener {
+            requireActivity().onBackPressedDispatcher.onBackPressed()
+        }
+        binding.etTitle.addTextChangedListener(
+            afterTextChanged = { s ->
+                isButtonEnable(!s.isNullOrBlank())
+            }
+        )
+        binding.ivPoster.setOnClickListener{
+            requestImage()
+        }
+        binding.btnCreatePlaylist.setOnClickListener {
+
+        }
     }
 
     override fun onResume() {
@@ -37,10 +97,25 @@ class CreatePlaylistFragment: Fragment() {
     }
 
     private fun hideBottomNav(){
-        (activity as BottomNavHider).hideBottomNav()
+        (requireActivity() as BottomNavHider).hideBottomNav()
     }
 
     private fun showBottomNav(){
-        (activity as BottomNavHider).showBottomNav()
+        (requireActivity() as BottomNavHider).showBottomNav()
+    }
+
+    private fun isButtonEnable(flag: Boolean){
+        binding.btnCreatePlaylist.isEnabled = flag
+    }
+
+    private fun requestImage(){
+        pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+    }
+
+    private fun showConfirmDialog() {
+        confirmDialog.show()
+    }
+    private fun showSnackBar(playlistTitle: String){
+        Snackbar.make(binding.root, "Плейлист $playlistTitle создан", Snackbar.LENGTH_SHORT).show()
     }
 }
